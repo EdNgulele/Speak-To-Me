14cc1ef9f645eca3f68da658f676637c4a3e3f5b
Edilson Ngulele <edilsonngulele@gmail.com>
2018-01-07 18:11:10 +0200

    first commit

---
+++ app/src/main/java/com/example/edblack/speaktome/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.edblack.speaktome
+
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.speech.RecognizerIntent
+import android.content.Intent
+import android.widget.Toast
+import android.content.ActivityNotFoundException
+import java.util.*
+import kotlin.collections.ArrayList
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val REQ_CODE_SPEECH_INPUT = 100
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        btn_speak.setOnClickListener {
+
+            speechInput()
+        }
+
+    }
+
+
+    /*
+    * Google Speech Input prompt (Voice)
+    * */
+
+    fun speechInput() {
+
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.speech_alert))
+
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT)
+        } catch (a: ActivityNotFoundException) {
+            Toast.makeText(applicationContext,
+                    getString(R.string.not_supported),
+                    Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+
+
+    /*
+    * Displaying the dialog input (Words)
+    * */
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+
+            REQ_CODE_SPEECH_INPUT -> if (resultCode == Activity.RESULT_OK && null != data) {
+                val result: ArrayList<String> = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                txt_speech_input.setText(result.get(0))
+            }
+
+        }
+    }
+
+
+}
+
